a387faab259392ae715cb13da418896283e03ea6
alekssenkou <aleksey.senkou@gmail.com>
2016-03-28 17:43:31 +0300

    Move compositions to a partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @featured_compositions = Composition.includes(:picture, members: :picture).first(9)
+    @compositions = Composition.includes(:picture, members: :picture).first(9)
   end
 end
