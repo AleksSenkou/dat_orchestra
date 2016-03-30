d934c20e2a016213e04a0b4697108bafcad6b6d4
alekssenkou <aleksey.senkou@gmail.com>
2016-03-30 19:02:35 +0300

    Add gallery_items path

---
+++ app/controllers/gallery_items_controller.rb
@@ -0,0 +1,5 @@
+class GalleryItemsController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   resources :repertoire, only: :index
+  resources :gallery_items, only: :index
 end
