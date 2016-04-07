130f52fda605a51e3ceabc81661aef90ab2cf014
alekssenkou <aleksey.senkou@gmail.com>
2016-04-07 16:04:46 +0300

    Remove bin from cap linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,14 +7,14 @@ set :deploy_to, '/home/deploy/dat_orchestra'
 set :rbenv_path, '/home/deploy/.rbenv'
 set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
-set :keep_releases, 15
+set :keep_releases, 25
 
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :passenger_restart_with_touch, true
 
