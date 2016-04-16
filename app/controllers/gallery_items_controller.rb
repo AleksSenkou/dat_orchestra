9835589bb1d8c7c71c59a17f295d067ff7b4b6e3
alekssenkou <aleksey.senkou@gmail.com>
2016-04-16 11:40:53 +0300

    Change gallery_items per page count

--- app/controllers/gallery_items_controller.rb
+++ app/controllers/gallery_items_controller.rb
@@ -1,6 +1,6 @@
 class GalleryItemsController < ApplicationController
   def index
     @gallery_items =
-      GalleryItem.includes(:translations).paginate(page: params[:page], per_page: 60)
+      GalleryItem.includes(:translations).paginate(page: params[:page], per_page: 50)
   end
 end
