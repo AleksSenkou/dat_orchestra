fddb491129312b58a5e2d7a2d1bff21c139f53ea
alekssenkou <aleksey.senkou@gmail.com>
2016-04-17 11:57:05 +0300

    Update AdminUsers in admin part

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -3,26 +3,34 @@ ActiveAdmin.register AdminUser do
 
   index do
     selectable_column
-    id_column
-    column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
+    column I18n.t('active_admin.labels.email'),         :email
+    column I18n.t('active_admin.labels.sign_in_count'), :sign_in_count
+    column I18n.t('active_admin.labels.created_at'),    :created_at
     actions
   end
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
+  filter :email, label: I18n.t('active_admin.labels.email')
+  filter :sign_in_count, label: I18n.t('active_admin.labels.sign_in_count')
+  filter :created_at, label: I18n.t('active_admin.labels.created_at')
 
   form do |f|
-    f.inputs "Admin Details" do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
+    f.inputs I18n.t('active_admin.necessary_information') do
+      f.input :email, label: I18n.t('active_admin.labels.email')
+      f.input :password, label: I18n.t('active_admin.labels.password')
+      f.input :password_confirmation, label: I18n.t('active_admin.labels.password_confirmation')
     end
     f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row(I18n.t('active_admin.labels.email')) { |b| b.email }
+      row(I18n.t('active_admin.labels.sign_in_count')) { |b| b.sign_in_count }
+      row(I18n.t('active_admin.labels.last_sign_in_at')) { |b| b.last_sign_in_at }
+      row(I18n.t('active_admin.labels.created_at')) { |b| b.created_at }
+      row(I18n.t('active_admin.labels.updated_at')) { |b| b.updated_at }
+    end
   end
 
 end
