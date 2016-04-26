161a241eed77f1e5b441566343472537a8fd81e2
alekssenkou <aleksey.senkou@gmail.com>
2016-04-26 12:28:59 +0300

    Add realtime compiled hero image

--- app/helpers/header_helper.rb
+++ app/helpers/header_helper.rb
@@ -1,4 +1,21 @@
 module HeaderHelper
+  def render_sass(file)
+    text = render("/layouts/#{ file }")
+    view_context = controller.view_context
+
+    engine = Sass::Engine.new(text, {
+      syntax: :sass,
+      cache: false,
+      read_cache: false,
+      style: :compressed,
+      sprockets:  {
+        context:     view_context,
+        environment: view_context.assets_environment
+      }
+    })
+    raw engine.render
+  end
+
   def heading
     uppercase_letters = @base_page.heading.scan(/\p{Upper}/).join
     normal_letters = @base_page.heading.delete uppercase_letters
